09f2aa5665b4a2c626e579cceefff6ff57b449e1
barbarian23 <xuanchien9a4@gmail.com>
2020-04-25 17:24:30 +0700

    second

---
+++ index.js
@@ -0,0 +1,8 @@
+const electron = require('electron');
+const puppeteer = require('puppeteer');
+var fs = require('fs');
+var xl = require('excel4node');
+
+const { app, BrowserWindow, ipcMain, Menu, dialog } = electron;
+
+
